341f606151d2838ace127c06ca26637e13bdd607
Vi Ramen <vi.is.chapmann@gmail.com>
2026-08-13 17:05:15 +0300

    fix: provide fallback implementation of os-specific lock strategy

--- src/os/lock/mod.rs
+++ src/os/lock/mod.rs
@@ -8,3 +8,8 @@ macro_rules! x {
 }
 
 x!("linux" linux);
+
+/// Fallback atomic implementation as this platform isn't supported by Resync.
+/// Same as [`crate::lock::Atomic`].
+#[cfg(not(any(target_os = "linux")))]
+pub type Os = crate::lock::Atomic;
